7af4e62cf386817e8ba631579c239e6d6c7506ef
atzscherlich <atzscherlich@PC17-096167.dresden.de>
2016-05-18 13:51:24 +0200

    Merge remote-tracking branch 'origin/master' Added domain classes: File.groovy && MediaAsset.groovy

---
+++ ftmedia-plugins/ftCore/grails-app/domain/ftcore/File.groovy
@@ -0,0 +1,32 @@
+package ftcore
+
+/**
+    @author:    atzscherlich
+    @version:   0.1
+    @date:      2016-05-18
+*/
+
+
+class File {
+    Long id
+    String path
+    String name
+    String extension
+    Integer size
+    String creator
+    Date dateAdded
+    Date lastModified
+    String checksum
+    
+    
+    static constraints = {
+        id {nullable: false}
+        path {nullable: false}
+        name {nullable: false}
+        extension {nullable: false}
+        size {nullable: false}
+        creator {nullable: false}
+        checksum {nullable: false}
+        
+    }
+}
---
+++ ftmedia-plugins/ftCore/grails-app/domain/ftcore/MediaAsset.groovy
@@ -0,0 +1,38 @@
+package ftcore
+
+/**
+    @author:    atzscherlich
+    @version:   0.1
+    @date:      2016-05-18
+*/
+
+class MediaAsset {
+    Long id
+    MediaType type
+    Integer playCount
+    Date lastPlayed
+    ParentalRating parentalRating
+    String tagline
+    
+    static hasOne = [file:File]
+    
+    static constraints = {
+        tagline {nullable: true}
+    }
+}
+
+class MediaType {
+    static final String MUSIC = 'Music'
+    static final String MOVIE = 'Movie'
+    static final String SERIES = 'Series'
+    static final String PICTURE = 'Picture'
+}
+
+class ParentalRating {
+    static final String Y = 'TV-Y'
+    static final String Y7 = 'TV-Y7'
+    static final String G = 'TV-G'
+    static final String PG = 'TV-PG'
+    static final String Y14 = 'TV-14'
+    static final String MA = 'TV-MA'
+}
