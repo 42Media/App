055933d2c1b105144601185c89a09b1c1bd2da99
sebX2 <sebastian_sebastian@gmx.net>
2016-06-08 16:20:00 +0200

    Controller for Testing and demonstrating LastFMRestService

---
+++ ftmedia-plugins/ftCore/grails-app/controllers/ftcore/TestLastFMRestServiceController.groovy
@@ -0,0 +1,16 @@
+package ftcore
+
+class TestLastFMRestServiceController {
+
+    def lastFMService
+
+    def renderGetMetaByArtist(){
+
+        render lastFMService.getMetaByArtist("Red Hot Chili Peppers")
+    }
+
+    def renderGetMetaByTrack(){
+
+        render lastFMService.getMetaByTrack("Eminem" ,"The real slim shady")
+    }
+}
