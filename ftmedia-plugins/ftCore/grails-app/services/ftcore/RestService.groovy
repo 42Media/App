9f7bdc5fe4b16a6ddd595ea69786dfba070991d4
atzscherlich <atzscherlich@gmail.com>
2016-07-03 08:37:36 +0200

    ftCore latest updates

--- ftmedia-plugins/ftCore/grails-app/services/ftcore/RestService.groovy
+++ ftmedia-plugins/ftCore/grails-app/services/ftcore/RestService.groovy
@@ -1,13 +1,8 @@
 package ftcore
 
-import grails.transaction.Transactional
-import grails.converters.JSON
-import org.grails.web.json.JSONObject
 import grails.plugins.rest.client.RestBuilder
 import grails.plugins.rest.client.RestResponse
 
-
-@Transactional
 class RestService {
 
     def getRequest(def url, def params) {
