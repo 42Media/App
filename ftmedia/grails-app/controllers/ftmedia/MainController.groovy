a5b4668f5542998847341efc90ba5f81d835e9be
atzscherlich <atzscherlich@gmail.com>
2016-07-04 20:59:37 +0200

    fixed music section on startpage show releases instead of tracks

--- ftmedia/grails-app/controllers/ftmedia/MainController.groovy
+++ ftmedia/grails-app/controllers/ftmedia/MainController.groovy
@@ -1,6 +1,7 @@
 package ftmedia
 
 import ftcore.security.User
+import ftlibrary.MusicService
 import ftlibrary.VideoAsset
 import ftlibrary.MusicAsset
 
@@ -12,14 +13,16 @@ class MainController {
     def index()
     {
         def user        = springSecurityService.currentUser
+        MusicService service = new MusicService()
 
-        def allVideos   = VideoAsset.findAllByUser(user, [max: 10])
-        def allMusic    = MusicAsset.findAllByUser(user, [max: 10])
+        def allVideos   = VideoAsset.findAll([max: 10])
+        def allMusic    = MusicAsset.findAll([max: 10])
+        def newMusic    = service.listReleases([max: 10])
 
         render (view: 'index', model: [
                 user: user,
                 videoList: allVideos,
-                musicList: allMusic
+                musicList: newMusic
         ])
     }
 }
