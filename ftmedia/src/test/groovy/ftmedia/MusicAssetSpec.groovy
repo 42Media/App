d1a501568b71e3f6c35f3727e5119325c14aac37
David Heenemann <support@xcluster.de>
2016-07-07 19:37:14 +0200

    add MusicAsset Unittest

--- ftmedia/src/test/groovy/ftmedia/MusicAssetSpec.groovy
+++ ftmedia/src/test/groovy/ftmedia/MusicAssetSpec.groovy
@@ -9,16 +9,21 @@ import ftlibrary.MusicAsset
 @Mock(MusicAsset)
 class MusicAssetControllerSpec extends Specification {
 
+    // Relevante Parameter setzen, die notwendig sind, um ein valides Modell von
+    // MusicAsset zu erzeugen
     def populateValidParams(params) {
         assert params != null
 
         params.title            = 'Test Music'
         params.trackArtist      = 'Test Music'
         params.release          = 2016
-        params.inserted         = new Date()
-        params.user             = new User(username: 'admin', password: 'password')
+        //params.inserted         = new Date()
+        //params.user             = new User(username: 'admin', password: 'password')
     }
 
+    // Es wird versucht ein Modell, ohne Übergabeparameter, zu erzeugen
+    // Verhält sich das Programm richtig, darf kein Modell erzeugt werden,
+    // und die Create View muss erneut eingeblendet werden
     void "test saving an invalid MusicAsset"() {
         when:
             request.contentType = FORM_CONTENT_TYPE
@@ -34,6 +39,9 @@ class MusicAssetControllerSpec extends Specification {
             view == 'create'
     }
 
+    // Es wird ein Modell mit gültigen Übergabeparametern erzeugt
+    // Verhält sich das Programm richtig, ist der Counter = 1, es wird auf die Startseite umgeleitet
+    // und eine Flash-Nachricht ausgegeben
     void "test saving a valid musicAsset"() {
         when:
             request.contentType = FORM_CONTENT_TYPE
